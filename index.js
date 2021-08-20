db1cf4b3e17e841b6cbc8d826ecb2a7a009c31bf
Sabine Adam <s80447@htw-dresden.de>
2021-08-20 22:11:12 +0200

    Farben + Links

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    //Farbe ändern
+
+    var color = Math.round(Math.random() * (4 - 0));
+
+    switch(color){
+        case 0:
+            document.getElementById('body').style.background="linear-gradient(rgba(255, 0, 0, 0.5), rgba(255, 0, 0, 0.5)), url(holz.jpg)";
+            break;
+        case 1:
+            document.getElementById('body').style.background="linear-gradient(rgba(0, 100, 0, 0.5), rgba(0, 100, 0, 0.5)), url(holz.jpg)";
+            break;
+        case 2:
+            document.getElementById('body').style.background="linear-gradient(rgba(75, 0, 130, 0.5), rgba(75, 0, 130, 0.5)), url(holz.jpg)";
+            break;
+        case 3:
+            document.getElementById('body').style.background="linear-gradient(rgba(0, 128, 128, 0.5), rgba(0, 128, 128, 0.5)), url(holz.jpg)";
+            break;
+        case 4:
+            document.getElementById('body').style.background="linear-gradient(rgba(255, 69, 0, 0.5), rgba(255, 69, 0, 0.5)), url(holz.jpg)";
+            break;
+        default:
+            document.getElementById('body').style.background="linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url(holz.jpg)";
+    }
+
+    //Copyright aktualisieren
+	var datum = new Date();
+    document.getElementById('year').innerHTML = datum.getFullYear();
+});
